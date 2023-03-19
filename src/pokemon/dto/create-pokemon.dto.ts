733e0b80d30bfabac3d2550cfc867734c27237b2
Tu Nombre <pisonigabriel@gmail.com>
2023-03-19 10:41:48 -0300

    feat:conectar mongodb crear schemas y validaciones post

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+    //isInt , isPositive , min1
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+    //isString , minLength
+    @MinLength(1)
+    @IsString()
+    name: string;
+
+}
